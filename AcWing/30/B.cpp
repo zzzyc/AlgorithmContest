323b7237b96a8f7fb1db2e415d68fc559d483eb0
zzzyc <44026067+zzzyc@users.noreply.github.com>
2021-12-18 20:18:38 +0800

    Update B.cpp

--- AcWing/30/B.cpp
+++ AcWing/30/B.cpp
@@ -16,16 +16,16 @@ void solve() {
 	n = strlen(s + 1);
 	
 	int res = 0;
-    for(int i = 1; i <= n; ++i) {
-        if(s[i] == ')') {
-            if(s[i - 1] == '(') dp[i] = dp[i - 2] + 2;
-            else {
-                int pre = i - 1 - dp[i - 1];
-                if(s[pre] == '(') dp[i] = dp[i - 1] + dp[pre - 1] + 2;
-            }
-        }
-        res = max(res, dp[i]);
-    }
+    	for(int i = 1; i <= n; ++i) {
+		if(s[i] == ')') {
+		    if(s[i - 1] == '(') dp[i] = dp[i - 2] + 2;
+		    else {
+			int pre = i - 1 - dp[i - 1];
+			if(s[pre] == '(') dp[i] = dp[i - 1] + dp[pre - 1] + 2;
+		    }
+		}
+		res = max(res, dp[i]);
+    	}
 	
 	int cnt = 0;
 	for(int i = 1; i <= n; ++i)
